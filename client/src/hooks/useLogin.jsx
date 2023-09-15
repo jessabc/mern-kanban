915cd6268b4caa897dc35a5c690edc8b279eb60f
jess <120062852+jessabc@users.noreply.github.com>
2023-09-15 10:53:29 +1000

    url

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -10,7 +10,7 @@ const useLogin = () => {
   
     const login = async (email, password) => {
         try {
-            const response = await axios.post('http://localhost:4000/api/user/login', {email, password})
+            const response = await axios.post('https://mern-kanban-backend.onrender.com/api/user/login', {email, password})
 
             dispatch({type: 'LOGIN', payload: response.data})
 
